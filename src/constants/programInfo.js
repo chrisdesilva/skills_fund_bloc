3f6b1c90b0d41de3c962dc920cc00f409ed21963
Chris DeSilva <chris.a.desilva@gmail.com>
2019-09-10 17:25:52 -0500

    Update GA ID

--- src/constants/programInfo.js
+++ src/constants/programInfo.js
@@ -26,7 +26,7 @@ export const threeStepCardText = {
 
 export const netlifyFormName = 'bloc_contact'
 
-export const GATracking = 'UA-99445258-41'
+export const GATracking = 'UA-68312423-1'
 
 // ***** END GENERAL SCHOOL INFO ***** 
 
