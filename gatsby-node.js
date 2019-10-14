e11cbc8227f5efcb68a01c40fc63c490a830facf
Chris DeSilva <chris.a.desilva@gmail.com>
2019-10-14 16:00:19 -0500

    Add redirects to gatsby-node

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,5 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+exports.createPages = ({ graphql, actions }) => {
+    const { createPage, createRedirect } = actions
 
-// You can delete this file if you're not using it
+    createRedirect({ fromPath: 'https://bloc-pt.skills.fund/*', toPath: 'https://bloc.skills.fund/:splat', isPermanent: true, force: true })
+}
