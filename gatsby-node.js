576a549b795f33b697b94671a1fca9dd5e0ceccd
unknown <chris.a.desilva@gmail.com>
2019-10-14 19:23:39 -0500

    Move all redirects to gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,10 @@
 exports.createPages = ({ graphql, actions }) => {
     const { createPage, createRedirect } = actions
-
+ 
     createRedirect({ fromPath: 'https://bloc-pt.skills.fund/*', toPath: 'https://bloc.skills.fund/:splat', isPermanent: true, force: true })
+    createRedirect({ fromPath: '/go', toPath: '/?utm_source=school&utm_medium=printedskfbrochure&utm_campaign=studentbrochure'})
+    createRedirect({ fromPath: '/accepted', toPath: '/'})
+    createRedirect({ fromPath: '/designer-track', toPath: '/'})
+    createRedirect({ fromPath: '/web-developer-track', toPath: '/'})
+   
 }
